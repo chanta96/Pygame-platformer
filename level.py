7c7585cd29c46414bd581656d7b088c6e875e852
Enzo Gonzalo Gramajo <112021063+chanta96@users.noreply.github.com>
2023-01-02 16:28:52 -0300

    screen moving with player

--- level.py
+++ level.py
@@ -3,7 +3,8 @@ from tiles import Tile
 from settings import *
 from player import Player
 
-
+SCREEN_MIN = screen_width/5
+SCREEN_MAX = screen_width - SCREEN_MIN
 class Level():
     def __init__(self, level_data, surface):
         # level setup
@@ -26,9 +27,25 @@ class Level():
                     player_sprite = Player((x, y))
                     self.player.add(player_sprite)
 
+    def scroll_x (self):
+        player = self.player.sprite
+        player_x = player.rect.centerx 
+        direction_x = player.direction.x
+
+        if player_x < SCREEN_MIN and direction_x < 0:
+            self.world_shift = 8
+            player.speed = 0
+        elif player_x > SCREEN_MAX and direction_x > 0:
+            self.world_shift = -8
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 8
     def run(self):
         # level tiles
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
         # player tiles
+        self.player.update()
         self.player.draw(self.display_surface)
+        self.scroll_x()
