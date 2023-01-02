b34b239e70a0ad378e78fc576b200229b56d2f68
Enzo Gonzalo Gramajo <112021063+chanta96@users.noreply.github.com>
2023-01-02 13:09:39 -0300

    last modification main with imports

--- main.py
+++ main.py
@@ -1,30 +1,27 @@
 import pygame
 from random import randint, choice
 from sys import exit
+from settings import *
+from tiles import Tile
+from level import Level
 
+# Iniciar pygame
 pygame.init()
-HEIGHT = 1200
-WIDTH = 700
 FPS = 60
-
-level_map = []
-
-
-size = (HEIGHT,WIDTH)
+size = ((screen_width, screen_height))
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode(size)
+level = Level(level_map, screen)
 
-game_active = True
 
-while game_active:
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            game_active = False
+
             pygame.quit()
             exit()
-    
     screen.fill('black')
+    level.run()
 
     pygame.display.update()
     clock.tick(FPS)
-
