a6d0ee90a27cd5d781bcf43af5d6b86d1c43507e
Enzo Gonzalo Gramajo <112021063+chanta96@users.noreply.github.com>
2023-01-02 13:07:19 -0300

    creating the level with a string, and tiles sizes

--- settings.py
+++ settings.py
@@ -1,3 +1,20 @@
 '''
 string for the map
 '''
+
+level_map = [
+    '                            ',
+    '                            ',
+    '                            ',
+    ' XX    XXX            XX    ',
+    ' XX P                       ',
+    ' XXXX         XX         XX ',
+    ' XXXX       XX              ',
+    ' XX    X  XXXX    XX  XX    ',
+    '       X  XXXX    XX  XXX   ',
+    '    XXXX  XXXXXX  XX  XXXX  ',
+    'XXXXXXXX  XXXXXX  XX  XXXX  ']
+
+tile_size = 64
+screen_width = 1200
+screen_height = tile_size*len(level_map)
